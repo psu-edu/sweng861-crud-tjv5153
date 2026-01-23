06efbe4f8bb40ced87805043835e177f9b458715
Tim Volkar <tjv5153@psu.edu>
2026-01-23 14:36:28 -0700

    Add frontend and update backend to support it

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,10 @@
 # Entry point for SWENG 861 CRUD project
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="../frontend/templates")
 
 @app.get("/health")
 def read_health():
@@ -10,3 +13,8 @@ def read_health():
 @app.get("/api/hello")
 def read_hello():
     return {"message": "Hello, World!"}
+
+@app.get("/", include_in_schema=False)
+@app.get("/posts", include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(request, "index.html")
